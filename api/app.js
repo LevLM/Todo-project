c58b9126319bb0c784d68da5b3ac3e9269ea48cb
Lisus Lev <lisus.lev@yandex.ru>
2022-12-22 15:04:04 +0200

    v2

--- api/app.js
+++ api/app.js
@@ -64,17 +64,30 @@ app.get('/tasks/:id', (req, res) => {
  let findTask = allTasks.find(t => t.id == id);
  res.json(findTask ? findTask : 'not found');
 });
- 
+
+let lastId = 3;
+function nextId() {
+     lastId++;
+     return lastId;
+}
+
 app.post('/tasks', (req, res) => {
  let descriptionField = req.body.description
  if ((descriptionField == '') || (descriptionField == null)) {
-   console.log('description error, incorrect value')
-   res.json('description error, incorrect value')
+    console.log('description error, incorrect value')
+    res.json('description error, incorrect value')
   } else {
-  console.log('add a new task ' + JSON.stringify(req.body));
-  const newTask = req.body
-  allTasks.push(newTask);
-  res.json();
+   if (lastId == 10000) {
+    console.log('error, the number of tasks is more than 10000')
+    res.json()
+   } else {
+    console.log('add a new task ' + JSON.stringify(req.body));
+    const newTask = req.body
+    newTask.id = nextId()
+    allTasks.push(newTask);
+    console.log(allTasks)
+    res.json();
+  }
  }
 });
 
@@ -98,8 +111,11 @@ app.listen(4000, () => {
 
 // Create new task throw curl:
 // curl -X POST -H "Content-Type: application/json" -d '{"description":"hello my new task", "created":"12/12/2013", "completed":"true", "id":"4"}' http://localhost:4000/tasks
+// tests for description:
 // curl -X POST -H "Content-Type: application/json" -d '{"created":"12/12/2013", "completed":"true", "id":"4"}' http://localhost:4000/tasks
 // curl -X POST -H "Content-Type: application/json" -d '{"description":"", "created":"12/12/2013", "completed":"true", "id":"4"}' http://localhost:4000/tasks
+// test for id:
+// curl -X POST -H "Content-Type: application/json" -d '{"description":"hello my new task", "created":"12/12/2013", "completed":"true"}' http://localhost:4000/tasks
 
 // get all completed tasks:
 // http://localhost:4000/tasks?completed=true%
